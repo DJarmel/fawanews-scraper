760a10e4af2ead162df18f9d481fc3fb38f0e277
DJarmel <david.jarmel@gmail.com>
2026-09-20 13:59:59 -0400

    Fix REFERER URL format in scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ from urllib.parse import urljoin
 
 
 BASE_URL = "http://www.fawanews.sc/"
-REFERER = "http://www.fawanews.sc"
+REFERER = "http://www.fawanews.sc/"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
